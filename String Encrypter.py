4bc5b0a5fe3882eed0a425161218629954777f63
Sashank <32218136+ShasV05@users.noreply.github.com>
2021-11-30 14:56:20 +0530

    Update String Encrypter.py

--- String Encrypter.py
+++ String Encrypter.py
@@ -4,14 +4,18 @@ nonEstring=list(input("Enter a word: "))
 
 def shiftDict(i):
     i = i % 26
-    alpha = str("ABCDEFGHIJKLMNOPQRSTUVWXYZ")
+    alpha = str("abcdefghijklmnopqrstuvwxyz")
     return dict(zip(alpha, alpha[i:] + alpha[:i]))
 
 Estring=[]
 key = randint(1,26)
 data = shiftDict(key)
 print(nonEstring)
+print(data)
 for i in nonEstring:
     Estring.append(data[i])
 print('unEncrypted:',nonEstring)
-print('paste these letters in decrypter \nEncrypted: ',Estring + "=-=" + str(key))
+Estr = ''
+for letter in Estring:
+  Estr +=letter
+print('paste these letters in decrypter \nEncrypted: ',Estr + "=-=" + str(key))
